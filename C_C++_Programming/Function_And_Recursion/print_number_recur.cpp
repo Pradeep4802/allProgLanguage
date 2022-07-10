02378d61771325eba30dce34a697d35ed632bf9a
Pradeep4802 <88763660+Pradeep4802@users.noreply.github.com>
2022-07-10 13:40:35 +0530

    Print_number_recursion

--- C_C++_Programming/Function_And_Recursion/print_number_recur.cpp
+++ C_C++_Programming/Function_And_Recursion/print_number_recur.cpp
@@ -1,18 +1,26 @@
 #include<iostream>
 using namespace std;
-int cnt = 1;
-void printNumber(int n) {
+// int cnt = 1;
+// void printNumber1(int n) {
+//     if(n <= 0)
+//         return;
+//     cout << cnt << endl;
+//     cnt++;
+//     printNumber(n-1);
+// }
+
+void printNumber2(int n) {
     if(n <= 0)
         return;
-    cout << cnt << endl;
-    cnt++;
-    printNumber(n-1);
+    cout << n << endl;
+    printNumber2(n-1);
 }
 
 int main() {
     int n;
     cout << "Enter the N : ";
     cin >> n;
-    printNumber(n);
+    // printNumber1(n);
+    printNumber2(n);
     return 0;
 }
