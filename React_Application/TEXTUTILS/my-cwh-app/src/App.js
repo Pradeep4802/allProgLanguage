8c0e6a68b57780f847847c7e932858d7e5cfa3b3
pradeepkumar <pradeep2904pal@gmail.com>
2022-09-06 00:13:17 +0530

    React and Portfolio Website

---
+++ React_Application/TEXTUTILS/my-cwh-app/src/App.js
@@ -0,0 +1,17 @@
+// import logo from './logo.svg';
+
+import "./App.css";
+import Navbar from "./components/Navbar";
+
+// let name = "Pradeep3";
+function App() {
+  return (
+    <>
+      {/* <Navbar title="TextUtils" aboutText = "About TextUtils" /> */}
+      {/* <Navbar /> */}
+      <Navbar title="TextUtils" />
+    </>
+  );
+}
+
+export default App;
---
+++ React_Application/TEXTUTILS/my-cwh-app/src/components/tempCodeRunnerFile.js
@@ -0,0 +1,2 @@
+
+  title: PropTypes.string,
