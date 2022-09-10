71d8e135c0694f8441405d40e3370f5f6b218f6c
pradeepkumar <pradeep2904pal@gmail.com>
2022-09-10 21:26:06 +0530

    React

--- React_Application/react-complete-guide/src/components/ExpenseItem.js
+++ React_Application/react-complete-guide/src/components/ExpenseItem.js
@@ -1,25 +1,32 @@
-import './ExpenseItem.css';
-import ExpenseDate from './ExpenseDate';
-import Card from './Card';
-import React from 'react';
+import React, { useState } from "react";
+import "./ExpenseItem.css";
+import ExpenseDate from "./ExpenseDate";
+import Card from "./Card";
 
+const ExpenseItem = (props) => {
+  // function clickHandler() {}
+  const [title, setTitle] = useState(props.title);
+  console.log('ExpensItem evaluated by React');
 
-function ExpenseItem(props) {
+  // let title = props.title;
 
-    // const expenseDate = new Date(2021, 2, 28);
-    // const expenseTitle = 'Car Insurance';
-    // const expenseAmount = 294.67;
+  const clickHandler = () => {
+    setTitle('Updated!');
+    // title = "Updated!";
+    console.log(title);
+  };
 
   return (
     <Card className="expense-item">
       {/* <div>{props.date.toISOString()}</div> */}
       <ExpenseDate date={props.date} />
       <div className="expense-item__description">
-        <h2>{props.title}</h2>
+        <h2>{title}</h2>
         <div className="expense-item__price">${props.amount}</div>
       </div>
+      <button onClick={clickHandler}>Change Title</button>
     </Card>
   );
-}
+};
 
 export default ExpenseItem;
