ce818004b1913c856275e58abc8c812d5a667c6d
pradeepkumar <pradeep2904pal@gmail.com>
2022-09-17 13:30:43 +0530

    Spotify clone

--- Python_Programming/MongoDB_229702/insert.py
+++ Python_Programming/MongoDB_229702/insert.py
@@ -1,12 +1,12 @@
 from logging import exception
 from pymongo import MongoClient
-client = MongoClient('localhost',27017)
-db = client.MCCP
+client = MongoClient("mongodb://localhost:27017/")
+db = client.PradMCC
 def insert():
     try:
         name1 = input("Enter the name: ")
         age1 = int(input("Enter the age: "))
-        db.mcc.insert({
+        db.myMcc.insert_one({
             "Name": name1,
             "Age": age1
         })
