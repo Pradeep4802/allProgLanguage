4b152761e43ddb025fcadd579cafb700c18d6414
pradeepkumar <pradeep2904pal@gmail.com>
2022-10-11 22:24:23 +0530

    Shell Scripting

---
+++ Shell_Scripting_Language/read_input.sh
@@ -0,0 +1,13 @@
+# ! /bin/bash
+
+echo "Enter Name : "
+read
+echo "Names : $REPLY"
+
+# echo "hello World!" | (
+#     read var1 var2
+#     echo $var1
+#     echo $var2
+# )
+
+# read -p "Enter the password: "$'\n' -s password
