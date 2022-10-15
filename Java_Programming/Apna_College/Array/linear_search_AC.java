d5022a80a8ac176f23c6858ee02cf6d678306de1
pradeepkumar <pradeep2904pal@gmail.com>
2022-10-15 22:50:10 +0530

    Linear_serach

--- Java_Programming/Apna_College/Array/linear_search_AC.java
+++ Java_Programming/Apna_College/Array/linear_search_AC.java
@@ -1,7 +1,26 @@
-package Java_Programming.Apna_College.Array;
+// package Java_Programming.Apna_College.Array;
 
 public class linear_search_AC {
+    public static int linearSearch(int numbers[], int key) {
+
+        for (int i = 0; i < numbers.length; i++) {
+            if (numbers[i] == key) {
+                return i;
+            }
+        }
+
+        return -1;
+    }
+
     public static void main(String[] args) {
-        
+        int numbers[] = { 2, 4, 6, 8, 10, 12, 14, 16 };
+        int key = 10;
+
+        int index = linearSearch(numbers, key);
+        if (index == -1) {
+            System.out.println("NOT found");
+        } else {
+            System.out.println("key is at index " + index);
+        }
     }
 }
