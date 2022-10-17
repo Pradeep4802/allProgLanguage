8cf573c9be6e18615baeb362924cd57434fefd67
pradeepkumar <pradeep2904pal@gmail.com>
2022-10-17 23:01:43 +0530

    Time complexity

--- Java_Programming/Apna_College/Array/linear_search_AC.java
+++ Java_Programming/Apna_College/Array/linear_search_AC.java
@@ -1,10 +1,10 @@
 // package Java_Programming.Apna_College.Array;
 
 public class linear_search_AC {
-    public static int linearSearch(int numbers[], int key) {
+    public static int linearSearch(String menu[], String key) {
 
-        for (int i = 0; i < numbers.length; i++) {
-            if (numbers[i] == key) {
+        for (int i = 0; i < menu.length; i++) {
+            if (menu[i] == key) {
                 return i;
             }
         }
@@ -14,9 +14,12 @@ public class linear_search_AC {
 
     public static void main(String[] args) {
         int numbers[] = { 2, 4, 6, 8, 10, 12, 14, 16 };
-        int key = 10;
+        String menu[] = { "dosa", "chole Bhature", "samosa", "chinese bhel", "Manchurian Bhel", "Ragda", "Dahi Puri" };
+        // int key = 20;
+        String key = "Ragda";
 
-        int index = linearSearch(numbers, key);
+        // int index = linearSearch(numbers, key);
+        int index = linearSearch(menu, key);
         if (index == -1) {
             System.out.println("NOT found");
         } else {
