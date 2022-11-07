b93260a98b855f4f571736761c1f7d006d82318f
pradeepkumar <pradeep2904pal@gmail.com>
2022-11-07 20:26:01 +0530

    Recursion

---
+++ Java_Programming/Apna_College/Recursion/sum_natural_num.java
@@ -0,0 +1,14 @@
+public class sum_natural_num {
+    public static int sum(int n) {
+        if(n == 1) {
+            return 1;
+        }
+        int sumn1 = sum(n-1);
+        int sum1 = n + sumn1;
+        return sum1;
+    }
+    public static void main(String[] args) {
+        int n = 5;
+        System.out.println(sum(n));;
+    }
+}
