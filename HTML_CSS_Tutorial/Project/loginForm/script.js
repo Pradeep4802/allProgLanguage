459f22f8c805addf6dcc9871b10ca527592062de
pradeepkumar <pradeep2904pal@gmail.com>
2022-11-18 21:38:52 +0530

    Login Form

--- HTML_CSS_Tutorial/Project/loginForm/script.js
+++ HTML_CSS_Tutorial/Project/loginForm/script.js
@@ -3,11 +3,7 @@ let signinBtn = document.getElementById("signinBtn");
 let nameField = document.getElementById("nameField");
 let title = document.getElementById("title");
 
-<<<<<<< HEAD
-signingBtn.onclick = function() {
-=======
 signinBtn.onclick = function() {
->>>>>>> 2bca116bd5f60181336b42ef6905cdae3406e6e6
     nameField.style.maxHeight = "0";
     title.innerHTML = "Sign In";
     signupBtn.classList.add("disable");
