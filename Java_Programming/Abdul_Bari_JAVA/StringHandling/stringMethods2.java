8a0a91e8e22bff966af8aa80c1416dd83467acb7
Pradeepkumar <pradeep2904pal@gmail.com>
2022-12-05 21:46:32 +0530

    Java string

---
+++ Java_Programming/Abdul_Bari_JAVA/StringHandling/stringMethods2.java
@@ -0,0 +1,16 @@
+public class stringMethods2 {
+    public static void main(String[] args) {
+        // String str1 = "Mr. Shahrukh Khan";
+        String str1 = "www.udemy.co.in";
+
+        System.out.println(str1.lastIndexOf("."));
+
+        // System.out.println(str1.endsWith("khan"));
+
+        // for (int i = 0; i < str1.length(); i++) {
+        //     System.out.print(str1.charAt(i) + " ");
+        // }
+
+        // System.out.println(str1.charAt(4));
+    }
+}
