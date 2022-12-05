88ce113e02644497fb1567629c8b0ce547968df9
Pradeepkumar <pradeep2904pal@gmail.com>
2022-12-05 20:56:10 +0530

    Java

---
+++ Java_Programming/Abdul_Bari_JAVA/StringHandling/stringMethods.java
@@ -0,0 +1,17 @@
+public class stringMethods {
+    public static void main(String[] args) {
+        String str = new String("netbeans");
+
+        // System.out.println(str);
+
+        // System.out.println(str.toUpperCase());
+
+        // str = str.trim();
+
+        // String str2 = str.substring(3,5);
+
+        String str2 = str.replace('e', 'M');
+
+        System.out.println(str2 + " " + str);
+    }
+}
---
+++ Java_Programming/Abdul_Bari_JAVA/StringHandling/stringObject.java
@@ -0,0 +1,22 @@
+public class stringObject {
+    public static void main(String[] args) {
+        // String str1 = "Java program";
+
+        // String str2 = new String("JAVA");
+
+        // char c[] = {'H','e','l','l','o'};
+
+        // String str3 = new String(c,1,4);
+
+        // byte b[] = {65,66,67,68};
+
+        // String str4 = new String(b,1,2);
+
+        // System.out.println(str3);
+
+        String str1 = "Java";
+        String str2 = new String("Java");
+
+        System.out.println(str1==str2);
+    }
+}
