6f5f50a204f1a8a710c8778672265a2fd1efbe62
Pradeepkumar <pradeep2904pal@gmail.com>
2022-12-20 23:06:43 +0530

    Now

--- C_C++_Programming/Love_Babbar/Recursion/reverseNumber.cpp
+++ C_C++_Programming/Love_Babbar/Recursion/reverseNumber.cpp
@@ -1,22 +1,30 @@
 #include <iostream>
 using namespace std;
 
-void print(int n) {
-    if(n == 0) {
+void print(int n)
+{
+    if (n == 0)
+    {
         return;
     }
+    for (int i = 0; i < n; i++)
+    {
+        cout << n << " ";
+        print(n - 1);
+        break;
+    }
     cout << n << " ";
-    print(n-1);
 }
 
-int main() {
+int main()
+{
     int n;
     cout << "Enter the value of n" << endl;
     cin >> n;
 
     cout << "Printing in Decreasing Order " << endl;
 
-    //function call
+    // function call
     print(n);
     cout << endl;
     return 0;
