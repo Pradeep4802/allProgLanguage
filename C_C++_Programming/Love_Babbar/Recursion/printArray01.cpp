60d03399e42382f9e78203c21c4299b34ca9619f
Pradeepkumar <pradeep2904pal@gmail.com>
2023-02-09 14:26:26 +0530

    Recursion

--- C_C++_Programming/Love_Babbar/Recursion/printArray01.cpp
+++ C_C++_Programming/Love_Babbar/Recursion/printArray01.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 using namespace std;
+// void revPrint(int arr[]. int index);
 
 void print(int arr[], int len, int index)
 {
@@ -9,18 +10,31 @@ void print(int arr[], int len, int index)
         return;
     }
     // ek case solve
+    print(arr, len, index+1);
     cout << arr[index] << ", ";
+    // index-=1;
 
     // Recursive call
-    print(arr, len, index + 1);
+}
+
+void revPrint(int arr[], int index) {
+    //base case
+    if(index < 0) {
+        return;
+    }
+    cout << arr[index] << ", ";
+
+    // R,R
+    revPrint(arr, index-1);
+
 }
 
 int main()
 {
-    int arr[] = {1, 3, 5, 9, 7};
+    int arr[] = {1, 3, 5, 7};
     int len = sizeof(arr) / sizeof(arr[0]);
-
-    print(arr, len, 0);
+    revPrint(arr, len-1);
+    // print(arr, len, 0);
     cout << endl;
     return 0;
 }
