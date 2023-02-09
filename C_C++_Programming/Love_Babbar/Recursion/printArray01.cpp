b8bd56956a18a7fa5d0654df9074813dcc6258fb
Pradeepkumar Pal <pradeep2904pal@gmail.com>
2023-02-09 15:50:18 +0530

    Recursion

--- C_C++_Programming/Love_Babbar/Recursion/printArray01.cpp
+++ C_C++_Programming/Love_Babbar/Recursion/printArray01.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <climits>
 using namespace std;
 // void revPrint(int arr[]. int index);
 
@@ -15,7 +16,7 @@ void print(int arr[], int len, int index)
     // index-=1;
 
     // Recursive call
-}
+}     
 
 void revPrint(int arr[], int index) {
     //base case
@@ -29,11 +30,37 @@ void revPrint(int arr[], int index) {
 
 }
 
+void print3(int arr[], int size) {
+    // base case 
+    if(size == 0) {
+        return;
+    }
+
+    cout << arr[0] << ", ";
+
+    // R.C
+    print3(arr+1, size-1);    
+}
+
+int getMax(int arr[], int size, int index) {
+    // base case
+    if(index == size)
+        return INT_MIN;
+
+    int ans = arr[index];
+    int recAns = getMax(arr, size, index+1);
+    return max(ans, recAns);
+}
+
 int main()
 {
-    int arr[] = {1, 3, 5, 7};
+    // int arr[] = {1, 3, 5, 7, 4, 6};
+    int arr[] = {7, 2, 15, 9, 21, 18};
     int len = sizeof(arr) / sizeof(arr[0]);
-    revPrint(arr, len-1);
+
+    cout << getMax(arr,len,0);
+    // print3(arr,len); 
+    // revPrint(arr, len-1);
     // print(arr, len, 0);
     cout << endl;
     return 0;
