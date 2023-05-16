0b8a7a719434514cb4e0611fe3b76ff1c3603700
Pradeepkumar Pal <pradeep2904pal@gmail.com>
2023-05-17 01:14:50 +0530

    Javascript

---
+++ Java_Programming/Abdul_Bari_JAVA/Variables/vari.java
@@ -0,0 +1,20 @@
+public class vari {
+    public static void main(String[] args) {
+        // byte b = 5;
+        // System.out.println(b);
+        // b=25;
+        // System.out.println(b);
+
+        byte b = 127;
+        short s = 300;
+        int i = 100;
+
+        float f = 25.3f;
+        char c = 'A';
+        System.out.println(b);
+        System.out.println(s);
+        System.out.println(i);
+        System.out.println(f);
+        System.out.println(c);
+    }
+}
